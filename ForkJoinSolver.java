707a9cf83c8b5a7fd7358de99cf5b419e155fc59
Simon Hammerlid <60077236+simhamm@users.noreply.github.com>
2024-03-16 19:41:34 +0100

    Update ForkJoinSolver.java

--- ForkJoinSolver.java
+++ ForkJoinSolver.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.Stack;
 import java.util.concurrent.ConcurrentSkipListSet;
-
+import java.util.concurrent.atomic.AtomicBoolean;
 /**
  * <code>ForkJoinSolver</code> implements a solver for
  * <code>Maze</code> objects using a fork/join multi-thread
@@ -23,6 +23,7 @@ public class ForkJoinSolver
     extends SequentialSolver
 {
 
+    AtomicBoolean first = new AtomicBoolean(false);
     protected ConcurrentSkipListSet<Integer> visited;
     protected ArrayList<ForkJoinSolver> forks;
     /**
@@ -57,6 +58,7 @@ public class ForkJoinSolver
     public ForkJoinSolver(Maze maze, int forkAfter)
     {
         this(maze);
+        first.set(true); 
     }
     @Override
     protected void initStructures(){
@@ -65,24 +67,6 @@ public class ForkJoinSolver
         frontier = new Stack<>(); //parallelisera?
         forks = new ArrayList<ForkJoinSolver>();
     }  
-
-    // How our forkjoinsolver should work:
-    // Each fork should know what nodes it has visited
-    // & each fork should update the ConcurrentSkipListSet when it visits a node, it is new: fork.
-
-    /*
-     * How the logic around how we choose to fork should work:
-     * 
-     * 1. If two or more neighbouring nodes have not been visited, then fork.
-     * 
-     * 2. One new fork should be created per non-visited neighbour if neighbour count is >= 2 (no need to
-     * fork if there only is one way to go).
-     * 
-     * 3. If all neighbours have been visited, then join.
-     * 
-     * 4. 
-     */
-
     /**
      * Searches for and returns the path, as a list of node
      * identifiers, that goes from the start node to a goal node in
@@ -116,53 +100,56 @@ public class ForkJoinSolver
                 GoalStatus.setGoalIsFound();
                 return pathFromTo(start, current);
             }
+
+            //add the "spawn point"/the first node visited in total to visited
+            if(first.get() == true){
+                visited.add(current);
+                first.set(false);
+            }
             
-            // Add the current node to the visited set
-            if(visited.add(current)){
-                
-                // Move player to the current node
-                maze.move(player, current);
-              
-                int neighborAmount = maze.neighbors(current).size();
-                // int unvisitedNeighbors = 0;
-                
-                if(neighborAmount >= 1){
-                    ArrayList<Integer> nonVisitedNodes = new ArrayList<Integer>();
-                    // Loop through the current node's neighbouring nodes and add them to a temporary list
-                    for(int nb : maze.neighbors(current)){
-                        if(!visited.contains(nb)){
-                            nonVisitedNodes.add(nb);
-                            predecessor.put(nb, current);
-                            // unvisitedNeighbors++;
+            // Move player to the current node
+            maze.move(player, current);
+            
+            int neighborAmount = maze.neighbors(current).size();
+
+            // Check if node has neighbors, aka, the node has 1 or more neighbors
+            if(neighborAmount >= 1){
+                ArrayList<Integer> nonVisitedNodes = new ArrayList<Integer>();
+                // Loop through the current node's neighboring nodes and add them to a temporary list
+                for(int nb : maze.neighbors(current)){
+                    if(visited.add(nb)){
+                        nonVisitedNodes.add(nb);
+                        predecessor.put(nb, current);
+                    }
+                }
+                //if there are 2 or more neighboring unvisited nodes, fork.
+                if(nonVisitedNodes.size() >= 2){
+                    for (Integer node : nonVisitedNodes) {
+
+                        //double-check that node is still available
+                        if(visited.contains(node)){
+                            // Create new fork
+                            ForkJoinSolver forkJS = new ForkJoinSolver(maze, node, visited);
+                            forks.add(forkJS);
+                            forkJS.fork();
                         }
                     }
-                    //if there are 2 or more neighbouring unvisited nodes, fork.
-                    if(nonVisitedNodes.size() >= 2){
-                        for (Integer node : nonVisitedNodes) {
-
-                            //double-check that node is still available
-                            if(!visited.contains(node)){
-                                ForkJoinSolver forkJS = new ForkJoinSolver(maze, node, visited);
-                                forks.add(forkJS);
-                                forkJS.fork();
-                            }
+                    List<Integer> workingPath = null;
+                    for(ForkJoinSolver processes : forks){
+                        List<Integer> path = processes.join();
+                        if(path != null){
+                            workingPath = path;
                         }
-                        List<Integer> workingPath = null;
-                        for(ForkJoinSolver processes : forks){
-                            List<Integer> path = processes.join();
-                            if(path != null){
-                                workingPath = path;
-                            }
-                        }
-                        if(workingPath != null) {
-                            List<Integer> tempList = pathFromTo(start, current);
-                            tempList.addAll(workingPath);
-                            return tempList;
-                        }
-                    } else { //when we have one unvisited neighbour
-                        if(nonVisitedNodes.size() != 0){
-                            frontier.push(nonVisitedNodes.get(0));
-                        }
+                    }
+                    if(workingPath != null) {
+                        List<Integer> tempList = pathFromTo(start, current);
+                        tempList.addAll(workingPath);
+                        return tempList;
+                    }
+                } else { //when we have one unvisited neighbor we don't create a new fork
+                        //instead we add that neighbor to the frontier
+                    if(nonVisitedNodes.size() != 0){
+                        frontier.push(nonVisitedNodes.get(0));
                     }
                 }
             }
@@ -173,7 +160,7 @@ public class ForkJoinSolver
     }
     
     /**
-    * InnerForkJoinSolver
+    * GoalStatus
     */
     public static class GoalStatus {
         static boolean goalIsFound = false;
@@ -186,18 +173,4 @@ public class ForkJoinSolver
             goalIsFound = true;
         }
     }
-
-    public static class DataHandler{
-        static protected ConcurrentSkipListSet<Integer> visited = new ConcurrentSkipListSet<>();
-
-        public static ConcurrentSkipListSet<Integer> getVisited(){
-            return visited;
-        }
-        
-        public static void addToSet(Integer visitedNode){
-            visited.add(visitedNode);
-        }
-    }
 }
-
-
